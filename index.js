34463bfc02abc7744c73271aa445e894d84ea641
Shine0111 <ranshine9@gmail.com>
2023-12-11 10:23:53 +0100

    rmv projectiles that hit the edges of the canvas

--- index.js
+++ index.js
@@ -104,8 +104,19 @@ function animate() {
   animationId = requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
   player.draw();
-  projectiles.forEach((projectile) => {
+  projectiles.forEach((projectile, index) => {
     projectile.update();
+    if (
+      projectile.x + projectile.radius < 0 ||
+      projectile.x - projectile.radius > canvas.width ||
+      projectile.y + projectile.radius < 0 ||
+      projectile.y - projectile.radius > canvas.width
+    ) {
+      // rmv from edges of the canvas
+      setTimeout(() => {
+        projectiles.splice(index, 1);
+      }, 0);
+    }
   });
   enemies.forEach((enemy, enemyIndex) => {
     enemy.update();
